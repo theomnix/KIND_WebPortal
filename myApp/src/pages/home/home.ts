8ae0f07c8a311b624c753439149b99ed4f43684d
Michael Huang <mihuan@microsoft.com>
2018-07-23 17:15:39 -0700

    Linked icons on home page to appropriate pages

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RightsPage } from '../rights/rights';
+import { ResponsibilitiesPage } from '../responsibilities/responsibilities';
+import { PlanPage } from '../plan/plan';
+import { YouthPage } from '../youth/youth';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  rightsPage = RightsPage;
+  responsibilitiesPage = ResponsibilitiesPage;
+  planPage = PlanPage;
+  youthPage = YouthPage;
 
   constructor(public navCtrl: NavController) {
 
