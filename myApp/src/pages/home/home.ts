a4b371796774aa9ebd5afdfa72bad28da5e131ec
Aaron Merrill <aamerril@microsoft.com>
2018-07-23 18:02:52 -0700

    Fixing a few things that broke post-merge.

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -20,16 +20,5 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-  navToRights(){
-      console.log("navToRights clicked")
-  }
-  navToResponsibilities(){
-    console.log("navToResponsibilities clicked")
-  }
-  navToPlan(){
-    console.log("navToPlan clicked")
-  }
-  navToYouth(){
-    console.log("navToYouth clicked")
-  }
+
 }
