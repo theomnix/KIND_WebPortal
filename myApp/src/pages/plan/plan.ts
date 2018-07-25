8a546c7aa20b5cb1cbba0b12be9b24be8b90beb9
Aaron Merrill <aamerril@microsoft.com>
2018-07-25 09:09:56 -0700

    Added toggles for the checkboxes.

--- myApp/src/pages/plan/plan.ts
+++ myApp/src/pages/plan/plan.ts
@@ -10,5 +10,15 @@ export class PlanPage {
   constructor(public navCtrl: NavController) {
 
   }
+  toggleBox(e: any){
+    if (e.path[0].attributes.checked.value == "false"){
+      e.path[0].attributes.checked.value = "true";
+      e.path[0].src = "/assets/imgs/Check.png";
+    }
+    else{
+      e.path[0].attributes.checked.value = "false";
+      e.path[0].src = "/assets/imgs/Box.png";
+    }
+  }
 
 }
